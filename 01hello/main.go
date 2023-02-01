822d3c95f93d97fe900d45345248dc2d77483f89
Mr-Emerald-Wolf <shivamsharma2002@gmail.com>
2023-02-01 23:31:44 +0530

    Hello World

---
+++ 01hello/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello from GO")
+	var username string = "rohan"
+	fmt.Printf("hello %s\n", username)
+	var isLoggedIn bool = false;
+	fmt.Println(isLoggedIn)
+	var smalLVal uint8 = 255;
+	fmt.Println(smalLVal)
+
+	// implicit style 
+	
+}
+
+
